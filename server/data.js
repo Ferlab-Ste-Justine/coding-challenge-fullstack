815b89ca478849992035efccdb69d77d31cb0af6
alexander-daniel <alex@cedarcedar.com>
2019-09-22 18:16:56 -0400

    server: create fake DB/data

---
+++ server/data.js
@@ -0,0 +1,18 @@
+// This would be in a DB
+let usersPasswords = {
+  'mika': 'foobar',
+  'sandy': 'beaches',
+  'fran': 'cisco'
+};
+
+// This would also be in a DB
+let messageState = {
+  'mika': 'You win some, you lose some.',
+  'sandy': 'Don\'t let the choice that you didn\'t make weigh you down.',
+  'fran': 'It is not the time which needs to be managed; it is ourselves.',
+};
+
+module.exports = {
+  usersPasswords,
+  messageState
+};
