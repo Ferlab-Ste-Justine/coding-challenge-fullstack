e7c71cb490b24115a1c688d13c2de78ef9fe7604
alexander-daniel <alex@cedarcedar.com>
2019-09-23 09:20:56 -0400

    add 2 more mock users to make the app look more full

--- server/data.js
+++ server/data.js
@@ -2,14 +2,18 @@
 let usersPasswords = {
   'mika': 'foobar',
   'sandy': 'beaches',
-  'fran': 'cisco'
+  'jebediah': '@123',
+  'fran': 'cisco',
+  'toni': '123abc'
 };
 
 // This would also be in a DB
 let messageState = {
   'mika': 'Eating lunch in the park today! 🌯🌲',
   'sandy': '🎵🎹 Listening to James Blake',
+  'jebediah': 'At La Ronde with the Kids 🎢🎡🎈',
   'fran': '"It is not the time which needs to be managed; it is ourselves." \n-Author Unknown',
+  'toni': 'Car problems. Running late -- start the meeting without me!'
 };
 
 module.exports = {
